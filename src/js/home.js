12833038a9fcd663b7c9f6433dfb88971461cb5d
Viraj Shah <shahvirajd@yahoo.com>
2015-10-04 22:25:39 -0400

    Date feature

--- src/js/home.js
+++ src/js/home.js
@@ -1,10 +1,11 @@
 var home = {
     getSentence: function() {
-        var priority, attendhost, eventname, output;
+        var priority, attendhost, eventname, date, output;
 
         priority = document.getElementById("sentence-priority").value;
         attendhost = document.getElementById("sentence-attendhost").value;
         eventname = document.getElementById("sentence-event").value;
+        date = document.getElementById("sentence-date").value.split("-");
 
         if (priority == "needto") priority = 1;
         else if (priority == "wantto") priority = 2;
@@ -13,7 +14,8 @@ var home = {
         output = {
             priority: priority,
             attend_or_host: attendhost,
-            event_name: eventname
+            event_name: eventname,
+            date: date
         };
 
         return output;
